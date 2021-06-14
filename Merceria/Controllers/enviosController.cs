6ba30494e2ff798cb89388c12e345cd017e6ef23
MEGPOID <85181848+MEGPOIDv02@users.noreply.github.com>
2021-06-13 22:09:40 -0500

    Agregar archivos de proyecto.

---
+++ Merceria/Controllers/enviosController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Merceria.Models;
+
+namespace Merceria.Controllers
+{
+    public class enviosController : Controller
+    {
+        private contextMerceria db = new contextMerceria();
+
+        // GET: envios
+        public ActionResult Index()
+        {
+            return View(db.Envios.ToList());
+        }
+
+        // GET: envios/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Envios envios = db.Envios.Find(id);
+            if (envios == null)
+            {
+                return HttpNotFound();
+            }
+            return View(envios);
+        }
+
+        // GET: envios/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: envios/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Id_Paqueteria,Id_Venta,Codigo_Rastreo,Fecha_Envio,Fecha_Estimada_Entrega,Estado_Envio,Fecha_Entrega,Estado")] Envios envios)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Envios.Add(envios);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(envios);
+        }
+
+        // GET: envios/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Envios envios = db.Envios.Find(id);
+            if (envios == null)
+            {
+                return HttpNotFound();
+            }
+            return View(envios);
+        }
+
+        // POST: envios/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Id_Paqueteria,Id_Venta,Codigo_Rastreo,Fecha_Envio,Fecha_Estimada_Entrega,Estado_Envio,Fecha_Entrega,Estado")] Envios envios)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(envios).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(envios);
+        }
+
+        // GET: envios/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Envios envios = db.Envios.Find(id);
+            if (envios == null)
+            {
+                return HttpNotFound();
+            }
+            return View(envios);
+        }
+
+        // POST: envios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Envios envios = db.Envios.Find(id);
+            db.Envios.Remove(envios);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Merceria/Controllers/perfilController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Merceria.Models;
+
+namespace Merceria.Controllers
+{
+    public class perfilController : Controller
+    {
+        private contextMerceria db = new contextMerceria();
+
+        // GET: perfil
+        public ActionResult Index()
+        {
+            return View(db.Usuarios.ToList());
+        }
+
+        // GET: perfil/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Usuarios usuarios = db.Usuarios.Find(id);
+            if (usuarios == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuarios);
+        }
+
+        // GET: perfil/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: perfil/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Nombre,Apellido_Paterno,Apellido_Materno,Fecha_Nacimiento,Telefono,Correo,Id_TipoUsuario,Username,Password,Estado")] Usuarios usuarios)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Usuarios.Add(usuarios);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(usuarios);
+        }
+
+        // GET: perfil/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Usuarios usuarios = db.Usuarios.Find(id);
+            if (usuarios == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuarios);
+        }
+
+        // POST: perfil/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Nombre,Apellido_Paterno,Apellido_Materno,Fecha_Nacimiento,Telefono,Correo,Id_TipoUsuario,Username,Password,Estado")] Usuarios usuarios)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(usuarios).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(usuarios);
+        }
+
+        // GET: perfil/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Usuarios usuarios = db.Usuarios.Find(id);
+            if (usuarios == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuarios);
+        }
+
+        // POST: perfil/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Usuarios usuarios = db.Usuarios.Find(id);
+            db.Usuarios.Remove(usuarios);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
